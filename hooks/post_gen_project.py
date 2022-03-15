c06726e044fc4b05969b45d2d70cb2cdda858e42
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-03-15 06:04:09 -0500

    fix paths to match new api location

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,6 @@ import os
 include_users = "{{ cookiecutter.include_users }}"
 
 if include_users != "yes":
-    shutil.rmtree("apps/users")
-    os.remove("docs/api/measurements.md")
-    os.remove("docs/api/authentication.md")
+    shutil.rmtree("api/apps/users")
+    os.remove("api/docs/api/measurements.md")
+    os.remove("api/docs/api/authentication.md")
